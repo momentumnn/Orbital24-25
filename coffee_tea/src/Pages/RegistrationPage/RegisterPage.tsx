858848dcd3fdbd1c88b3dfd3aa1024c78e16ea1f
e1384467 <e1384467@u.nus.edu>
2025-05-21 04:14:41 +0800

    Added RegisterPage with registerpage css

---
+++ coffee_tea/src/Pages/RegistrationPage/RegisterPage.tsx
@@ -0,0 +1,86 @@
+import React , {useState} from "react";
+import supabase from "../SupabaseAuthentication/SupabaseClient";
+import { Link } from "react-router";
+import './RegisterPage.css';
+
+
+function RegisterPage(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setMessage("");
+        const { data, error }= await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+        if (error) {
+            setMessage(error.message);
+            return;
+        }
+
+        if (data) {
+            setMessage("User account created!");
+        }
+
+        setEmail("");
+        setPassword("");
+    };
+
+    return (
+        <div className="page">
+
+            <form onSubmit={handleSubmit}
+            className="box" >
+                <h1 className="title">
+                    Register Now!
+                </h1>
+
+             
+                {message && <span>{message}</span>}
+                <div className="email-input">
+                    <label className = "email-text"> Email: </label>
+                    <input 
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className="email" type="email" placeholder="Email here..." 
+                    required />
+                </div>
+
+                <div className = "password-input">
+                    <label className = "password-text"> Password: </label>
+                    <input 
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className="password" type="password" placeholder="Password here..." 
+                    required />
+                 
+
+                </div>
+
+                <div className="center">
+                    <button className="button" type="submit">Register!</button>
+                </div>
+
+                <p className="redirect">Already have an account?<Link to="/Login">Login</Link>
+                </p>
+            </form>
+
+        </div>
+
+
+
+
+
+
+
+
+    );
+        
+    
+}
+
+export default RegisterPage;
