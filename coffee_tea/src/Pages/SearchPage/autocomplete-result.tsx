0606ddea629e095a2c1427a8c6955c6dccc6c5ea
momentumnn <114093710+momentumnn@users.noreply.github.com>
2025-06-16 23:45:20 +0800

    button to add to saved

--- coffee_tea/src/Pages/SearchPage/autocomplete-result.tsx
+++ coffee_tea/src/Pages/SearchPage/autocomplete-result.tsx
@@ -11,42 +11,80 @@ import supabase from "../../SupabaseAuthentication/SupabaseClient";
 interface Props {
   place: google.maps.places.Place | null;
 }
+
 const AutocompleteResult = ({ place }: Props) => {
   const map = useMap();
   const [infowindowOpen, setInfowindowOpen] = useState(true);
   const [markerRef, marker] = useAdvancedMarkerRef();
   const [photo, setPhoto] = useState<string | null>();
+  const [id, setId] = useState<string>();
 
-  // adjust the viewport of the map when the place is changed
+  const [save, setSave] = useState<boolean>(false);
+
   useEffect(() => {
+    // adjust the viewport of the map when the place is changed
     if (!map || !place) return;
     if (place.viewport) map.fitBounds(place.viewport);
     if (place && place.photos && place.photos.length > 0) {
-      // Use the correct method: getUrl()
       setPhoto(place.photos[0].getURI());
     } else {
-      setPhoto(null); // Clear photo if no photos or place is null
+      setPhoto(null);
     }
-    const updateRestaurant = async () => {
-      const { error } = await supabase.from("restaurants").upsert(
-        {
-          displayName: place.displayName,
-          formattedAddress: place.formattedAddress,
-          image_url: photo,
-          places_api_id: place.id,
-          rating: place.rating,
-          regularOpeningHours: place.regularOpeningHours,
-          reviews: place.reviews,
-        },
-        { onConflict: "places_api_id" }
-      );
-      if (error) console.log(error);
-    };
-    updateRestaurant();
   }, [map, place]);
   if (!place) return null;
+  const updateRestaurant = async () => {
+    const { error } = await supabase.from("restaurants").upsert(
+      {
+        displayName: place.displayName,
+        formattedAddress: place.formattedAddress,
+        image_url: photo,
+        places_api_id: place.id,
+        rating: place.rating,
+        regularOpeningHours: place.regularOpeningHours,
+        reviews: place.reviews,
+        latlng: place.location,
+      },
+      { onConflict: "places_api_id" }
+    );
+    if (error) console.log(error);
+  };
+  updateRestaurant();
+  const getRestaurantId = async () => {
+    const { data, error } = await supabase
+      .from("restaurants")
+      .select("id")
+      // user.id is from the auth table
+      // user_id in user_saves table is equal to the uuid of the current logged in user.
+      .eq("places_api_id", place.id);
 
-  console.log(place.rating);
+    if (error) {
+      console.error("Error fetching saved restaurants:", error.message);
+      return;
+    }
+    // console.log(data[0].id);
+    setId(data[0].id);
+  };
+  getRestaurantId();
+  const updateSave = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    //no user so i stop
+    if (!user) return;
+
+    const { error } = await supabase.from("user_saves").upsert(
+      {
+        user_id: user.id,
+        visited: false,
+        places_api_id: place.id,
+        restaurant_id: id,
+      },
+      { ignoreDuplicates: false }
+    );
+    if (error) console.log(error);
+  };
+
+  // console.log(place.location);
 
   // add a marker for the selected place
   return (
@@ -81,6 +119,7 @@ const AutocompleteResult = ({ place }: Props) => {
                 style={{ width: "250px" }}
               ></img>
             )}
+            <button onClick={() => updateSave()}> save to profile</button>
           </div>
         </InfoWindow>
       )}
